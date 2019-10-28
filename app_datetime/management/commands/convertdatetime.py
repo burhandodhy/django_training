b1f5942a83e11200c2a800a4d91abe641d4de5e3
burhandodhy <burhandodhy@gmail.com>
2019-10-28 13:52:20 +0500

    Minor change

--- app_datetime/management/commands/convertdatetime.py
+++ app_datetime/management/commands/convertdatetime.py
@@ -1,4 +1,4 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from app_datetime.models import DateTime
 from datetime import datetime
 import pytz
